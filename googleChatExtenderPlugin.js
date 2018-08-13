cbdb0bb1be93f7a4be70a82504701d8549a6c589
Toinane <toinane@crea-that.fr>
2018-08-13 18:09:34 +0200

    Update googleChatExtenderPlugin.js

--- googleChatExtenderPlugin.js
+++ googleChatExtenderPlugin.js
@@ -4,6 +4,29 @@ let gce = {
   plugins: {}
 };
 
+const googleChatExtenderCss = `
+    #custom-button:hover{ background: #F5F5F5; }
+    #custom-box{ z-index: 1000; display: flex; align-items: center; justify-content: center; position: absolute; top: 0; left: 0; width: 100vw; height: 100vh;}
+    #custom-zone{ z-index: 999; position: absolute; top: 0; left: 0; width: 100vw; height: 100vh; background: rgba(0, 0, 0, 0.5); }
+    #custom-menu{ z-index: 1001; position: relative; background: rgb(240, 240, 240); width: 80vw; height: 90vh; border-radius: 5px; box-shadow: 0 12px 15px 0 rgba(0,0,0,0.24); overflow: auto; }
+    #custom-menu h1{ padding: 15px 37px; font-weight: normal; font-size: 1.4em; background: white; }
+    .custom-separator{ padding: 15px 20px; border-top: 1px solid #e0e0e0; }
+    .plugin{ padding: 10px 20px; position: relative; background:white; margin: 10px 0; border-radius: 2px; box-shadow: 0px 1px 4px 1px #dedede; }
+    .plugin h2{ font-size: 1.3em; margin-bottom: 5px; }
+    .plugin h2 span{ font-size: 0.8em; color: rgb(100, 100, 100); }
+    .plugin h3{ color: rgb(142, 142, 142); font-size: 0.9em; }
+    .plugin-enabled{ position: absolute; top: 10px; right: 20px; }
+    .plugin h4{ font-size: 1.1em; margin: 12px 0; }
+    .plugin-variables div{ display: inline-block; margin: 10px; }
+    .plugin-variables input{ margin-top: 5px; width: 200px; padding: 5px 3px; font-size: 0.8em; border-radius: 2px; border: none; background: #e4e4e4;}
+    .plugin-variables textarea{ padding: 5px; font-size: 0.8em; border: none; border-radius: 2px; background: rgb(225, 225, 225); width: 50vw; resize: vertical; }
+    .var-name label{ text-transform: capitalize; margin-right: 10px; }
+    .var-name span{ font-size: 0.8em; color: rgb(100, 100, 100); }
+    #custom-text{ margin-top: 10px; width: 45vw; min-height: 150px; border: none; background: #E1E1E1; padding: 5px; border-radius: 2px; resize: none; }
+    #custom-save{ display: inline-block; float: right; margin-right: 20px; padding: 10px 40px; margin-bottom: 30px; border-radius: 3px; color: #00897b; font-weight: bold; font-size: 0.9em; text-transform: uppercase; cursor: pointer; }
+    #custom-save:active, #custom-save:focus{ background: rgba(0,137,123,0.2); }
+  `;
+
 function log(text, type) {
   let css = {
     'warn': 'color: red; background: yellow;text-transform:uppercase;font-size:2em;',
@@ -22,17 +45,18 @@ function getGCE() {
 }
 
 function addGCEOption() {
-  const optionsElement = document.querySelector('.JAPqpe');
-  const item = `
-    <content class="z80M1" id="custom-button" jsaction="click:o6ZaF(preventDefault=true); mousedown:lAhnzb; mouseup:Osgxgf; mouseenter:SKyDAe; mouseleave:xq3APb; touchstart:jJiBRc; touchmove:kZeBdd; touchend:VfAz8(preventMouseEvents=true)">
-      <div class="uyYuVb">
-        <div class="j07h3c">Google Chat Extender</div>
-      </div>
-    </content>
-    <div class="kCtYwe" role="separator"></div>
-  `;
+  const optionsElement = document.querySelector('.qC1oJf');
+  let item = document.createElement('content');
+  item.classList.add('z80M1');
+  item.id = 'custom-button';
+  item.innerHTML = '<div class="uyYuVb"><div class="j07h3c">Google Chat Extender</div></div>';
 
-  optionsElement.innerHTML = item + optionsElement.innerHTML;
+  let style = document.createElement('style');
+  style.innerHTML = googleChatExtenderCss;
+  document.head.insertBefore(style, document.head.lastChild);
+
+  optionsElement.parentElement.insertBefore(item, optionsElement);
+  optionsElement.parentElement.insertBefore(document.querySelector('.kCtYwe').cloneNode(), optionsElement);
   document.getElementById('custom-button').addEventListener('click', createMenu);
 }
 
@@ -137,14 +161,21 @@ function saveConfiguration(inputs) {
     gce.plugins[input.id] = input.value;
   });
   localStorage.setItem('gce', JSON.stringify(gce));
+
+  window.location.reload();
 }
 
 function launchPlugins() {
   log('initializing plugins', 'warn');
   plugins.forEach(plugin => {
     if(!plugin.enabled) return;
-    eval(plugin.content);
-    log(plugin.name + ' is loaded!', 'ok');
+    try {
+      eval(plugin.content);
+      log(plugin.name + ' is loaded!', 'ok');
+    }
+    catch (err) {
+      log(plugin.name + 'is crashed: ' + err, 'error');
+    }
   })
 }
 
